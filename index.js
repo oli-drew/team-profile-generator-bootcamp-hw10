002cc8591cd05a3c15dd77d984a3bcb3dcf3a283
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-07 22:02:29 +0000

    Use chalk, figlet and inquirer to introduce the application

--- index.js
+++ index.js
@@ -7,3 +7,65 @@ const emailValidator = require("email-validator");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
+
+// Exit application
+const exitGenerator = (message) => {
+  console.log(
+    chalk.yellow(
+      figlet.textSync(message, {
+        font: "Small",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+    )
+  );
+};
+
+// Ask the user if they would like to start or exit
+const startGenerator = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "start",
+        message: "Shall we start building your team?",
+        choices: ["Yes, Please!", "Nah, I'm a lone wolf!"],
+      },
+    ])
+    .then((data) => {
+      const answer = data.start;
+      if (answer === "Yes, Please!") {
+        console.log(chalk.green.bold("Okay, let's do this!", "\n"));
+        // Start asking questions
+        collectInfo();
+      } else {
+        exitGenerator("Bye Bye!");
+      }
+    });
+};
+
+// Introduce Application
+const applicationIntro = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync("Team Profile Generator", {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+    )
+  );
+  console.log(
+    chalk.bgGreen.bold.white(
+      "----- Generate a HTML webpage to display the structure of your software team -----",
+      "\n"
+    )
+  );
+  startGenerator();
+};
+
+applicationIntro();
+
+const collectInfo = function () {
+  console.log("Ask questions");
+};
