d6b924a7c0cdb03219fcae1224000b6bc5cce017
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-09 09:16:03 +0000

    Function to generate html file working

--- index.js
+++ index.js
@@ -75,25 +75,25 @@ const managerInfo = async (userInputs = []) => {
   const questions = [
     {
       type: "input",
-      name: "managerName",
+      name: "name",
       message: "Hi, what is the team manager's name?",
       validate: validateResponse,
     },
     {
       type: "input",
-      name: "managerID",
+      name: "id",
       message: "What is the employee ID of the manager?",
       validate: validateResponse,
     },
     {
       type: "input",
-      name: "managerEmail",
+      name: "email",
       message: "What is the manager's email address?",
       validate: validateEmail,
     },
     {
       type: "input",
-      name: "managerOfficeNumber",
+      name: "officeNumber",
       message: "What is the manager's office number?",
       validate: validateResponse,
     },
@@ -115,16 +115,16 @@ const managerInfo = async (userInputs = []) => {
 async function init() {
   const inputs = await managerInfo();
   const managerProfile = new Manager(
-    inputs[0].managerName,
-    inputs[0].managerID,
-    inputs[0].managerEmail,
-    inputs[0].managerOfficeNumber
+    inputs[0].name,
+    inputs[0].id,
+    inputs[0].email,
+    inputs[0].officeNumber
   );
   team.push(managerProfile);
   if (inputs[0].nextOption === "Add Employee") {
     addEmployee();
   } else {
-    generateHTML();
+    generateTeam();
   }
 }
 
@@ -234,10 +234,151 @@ async function addEmployee() {
   } else {
     // Make the html profile
     console.log(chalk.green.bold("HTML Profile Generated!"));
-    generateHTML();
+    generateTeam();
   }
 }
 
-const generateHTML = function () {
-  console.log(`Team Array: ${team}`);
+const generateTeam = function () {
+  console.log(chalk.green.bold("Your team has been generated!"));
+  fs.writeFileSync("./dist/sample.html", generateHTML(), "utf-8");
+};
+
+// Manager card
+const createManager = (manager) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <div class="card-header bg-primary text-white">
+          <h5 class="card-title">${manager.name}</h5>
+          <h6 class="mb-0">
+            <span class="material-icons align-middle"> groups </span>
+            <span>Manager</span>
+          </h6>
+        </div>
+        <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: <span>${manager.id}</span></li>
+            <li class="list-group-item">
+              Email:
+              <a href="mailto:${manager.email}"
+                >${manager.email}</a
+              >
+            </li>
+            <li class="list-group-item">Office Number: <span>${manager.officeNumber}</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+};
+
+// Engineer card
+const createEngineer = (engineer) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <div class="card-header bg-primary text-white">
+          <h5 class="card-title">${engineer.name}</h5>
+          <h6 class="mb-0">
+            <span class="material-icons align-middle"> engineering </span>
+            <span>Engineer</span>
+          </h6>
+        </div>
+        <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: <span>${engineer.id}</span></li>
+            <li class="list-group-item">
+              Email:
+              <a href="mailto:${engineer.email}"
+                >${engineer.email}</a
+              >
+            </li>
+            <li class="list-group-item">GitHub Profile: <a href="https://www.github.com/${engineer.github}" target="_blank"> ${engineer.github}</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+};
+
+// Intern card
+const createIntern = (intern) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <div class="card-header bg-primary text-white">
+          <h5 class="card-title">${intern.name}</h5>
+          <h6 class="mb-0">
+            <span class="material-icons align-middle"> school </span>
+            <span>Intern</span>
+          </h6>
+        </div>
+        <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: <span>${intern.id}</span></li>
+            <li class="list-group-item">
+              Email:
+              <a href="mailto:${intern.email}"
+                >${intern.email}</a
+              >
+            </li>
+            <li class="list-group-item">School: <span>${intern.school}</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+};
+
+// Generate cards HTML
+const buildTeam = (team) => {
+  let htmlCards = "";
+  team
+    .filter((employee) => employee.getRole() === "Manager")
+    .map((manager) => (htmlCards += createManager(manager)));
+  team
+    .filter((engineer) => engineer.getRole() === "Engineer")
+    .map((engineer) => (htmlCards += createEngineer(engineer)));
+  team
+    .filter((intern) => intern.getRole() === "Intern")
+    .map((intern) => (htmlCards += createIntern(intern)));
+  return htmlCards;
+};
+
+// Generate HTML
+const generateHTML = () => {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Profile</title>
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <header class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Team Profile</h1>
+        <p class="col-md-8 fs-4">
+          Information about my software engineering team
+        </p>
+      </div>
+    </header>
+    <!-- Card container -->
+    <main class="container pb-4">
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <!-- Cards here -->
+        ${buildTeam(team)}
+      </div>
+    </main>
+  </body>
+</html>`;
 };
